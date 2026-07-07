7900ed15009c7b00c8d6f1d39b1f8ae7d5800e6a
jy07023918-eng <jy07023918@gmail.com>
2026-07-07 16:40:35 +0900

    Add Firebase config

--- firebase.js
+++ firebase.js
@@ -1,1 +1,41 @@
-// Firebase 연결 파일
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  serverTimestamp,
+  query,
+  orderBy
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD1kzRrpxzHkY65p8qfpD7KScJ-7EurF98",
+  authDomain: "work-manual-9e4cd.firebaseapp.com",
+  projectId: "work-manual-9e4cd",
+  storageBucket: "work-manual-9e4cd.firebasestorage.app",
+  messagingSenderId: "844623387068",
+  appId: "1:844623387068:web:db37d7d247a7bef3dddb3b9",
+  measurementId: "G-RCNSXDPR7G"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const manualCollection = collection(db, "work-manual");
+const manualQuery = query(manualCollection, orderBy("updatedAt", "desc"));
+
+export {
+  db,
+  manualCollection,
+  manualQuery,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  serverTimestamp
+};
