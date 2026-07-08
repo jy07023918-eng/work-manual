76322c33d4c8e9a8ebce5e1c312072af3edd0683
jy07023918-eng <jy07023918@gmail.com>
2026-07-08 16:09:50 +0900

    Update app.js

--- app.js
+++ app.js
@@ -20,20 +20,8 @@ const quill = new Quill("#editor", {
       ["bold", "italic", "underline"],
       [{ background: [] }, { color: [] }],
       [{ list: "bullet" }, { list: "ordered" }],
-      [{ indent: "-1" }, { indent: "+1" }],
       ["clean"]
-    ],
-    keyboard: {
-      bindings: {
-        tab: {
-          key: 9,
-          handler: function () {
-            this.quill.format("indent", "+1");
-            return false;
-          }
-        }
-      }
-    }
+    ]
   }
 });
 
